feafe54501d8b9b9114b2fe5e01a89ccdb97db2e
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-05-11 22:11:12 +0300

    practice

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,14 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees as parkTrees, wildlife as parkWildlife} from "./parks/RockyMountain"
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
-
+  <div>
+  howManyParks(); 
+<parkTrees/>
+<parkWildlife/>
+<MesaVerde/>
+</div>
   return <h1>Colorado State Parks!</h1>;
 }
